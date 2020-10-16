5f8a3074f2cf9107a4f77013a384c9dffc57daa1
Miha Eržen <miha.erzen1@gmail.com>
2020-10-16 13:40:15 +0300

    Add images support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from 'next'
 
+import beeImage from '~assets/img/getty-bees-honey.jpg'
+
 import Layout from '../components/Layout'
 
 const IndexPage: NextPage = () => (
   <Layout title="BeHoney">
     <h1>Hello 🐝</h1>
+
+    <img alt="Bee" src={beeImage} />
   </Layout>
 )
 
